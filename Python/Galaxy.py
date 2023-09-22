9493749046318c097b9b4d1e0d24323af1a2db02
Sameer-ctrl <itssameer.patidar@gmail.com>
2023-09-22 23:21:44 +0530

    Auto

--- Python/Galaxy.py
+++ Python/Galaxy.py
@@ -9,7 +9,7 @@ from astropy.coordinates import SkyCoord
 plt.style.use('my_style.mpl')
 
 cosmo=FlatLambdaCDM(H0=69.6,Om0=0.3,Tcmb0=2.725)
-z_abs=0.347
+z_abs=0.347579
 
 dA_scale=cosmo.kpc_proper_per_arcmin(z_abs)
 
@@ -51,22 +51,22 @@ ra_qso,dec_qso=[1.4968167,16.163614]
 # plt.show()
 # quit()
 
-abs_sys_coord=SkyCoord(ra_qso*u.degree,dec_qso*u.degree)
-gal_coords=SkyCoord(ra*u.degree,dec*u.degree)
+# abs_sys_coord=SkyCoord(ra_qso*u.degree,dec_qso*u.degree)
+# gal_coords=SkyCoord(ra*u.degree,dec*u.degree)
 
-d2d = abs_sys_coord.separation(gal_coords)
+# d2d = abs_sys_coord.separation(gal_coords)
 
 # print((d2d*u.deg*60)[0].value)
 # quit()
 
-for i in range(len(ra)):
-    sep=(d2d*u.deg*60)[i].value
-    proj_dist=dA_scale.value*sep*0.001
-    print(f'{ra[i]:.5f}  &  {dec[i]:.5f}  &  {z[i]}  &  {v[i]:.0f}  &  {sep:.1f}  &  {proj_dist:.1f}  \\\\')
+# for i in range(len(ra)):
+#     sep=(d2d*u.deg*60)[i].value
+#     proj_dist=dA_scale.value*sep*0.001
+#     print(f'{ra[i]:.5f}  &  {dec[i]:.5f}  &  {z[i]}  &  {v[i]:.0f}  &  {sep:.1f}  &  {proj_dist:.1f}  \\\\')
 
 
 
-quit()
+# quit()
 
 # mask_sep = d2d < 1*u.deg
 
@@ -83,12 +83,12 @@ quit()
 #     else:
 #         mask.append(False)
 
-print(len(z[mask_sep]),len(z))
+# print(len(z[mask_sep]),len(z))
 
 
 
 
-quit()
+# quit()
 
 
 # sep_sky=abs_sys_coord.separation(gal_coords).arcminute
@@ -104,11 +104,11 @@ def circle(r,x0,y0,label):
 
     plt.plot(x,y,ls='--',label=label)
 
-print(abs(v))
+# print(abs(v))
 
 plt.figure(figsize=(16,13))
 
-plt.scatter((ra-ra_qso)*60,(dec-dec_qso)*60,s=50,c=abs(v),cmap='jet')
+plt.scatter((ra-ra_qso)*60,(dec-dec_qso)*60,s=600,c=abs(v),cmap='jet')
 
 # for i in range(len(ra)):
 #     plt.text((ra[i]-ra_qso)*60,(dec[i]-dec_qso)*60,s=f'{i+1}')
@@ -119,16 +119,16 @@ cb.set_label(r'$\mathbf{\Delta V \ (km \ s^{-1})}$',labelpad=40)
 circle(500/dA_scale.value,0,0,r'$\mathbf{500 \ kpc}$')
 circle(1000/dA_scale.value,0,0,r'$\mathbf{1 \ Mpc}$')
 # circle(5000/dA_scale.value,0,0,'5 Mpc')
-plt.scatter(0,0,s=200,c='black',marker='X')
-plt.legend(fontsize=30)
-plt.xlim(-15,15)
-plt.ylim(-15,15)
+plt.scatter(0,0,s=900,c='black',marker='X')
+plt.legend(fontsize=30,loc='lower right')
+plt.xlim(-10,10)
+plt.ylim(-15,5)
 plt.xticks(fontsize=25)
 plt.yticks(fontsize=25)
 plt.xlabel(r'$\mathbf{\alpha-{\alpha}_{QSO} \ [arc \ min]}$',fontsize=30,labelpad=15)
 plt.ylabel(r'$\mathbf{\delta-{\delta}_{QSO} \ [arc \ min]}$',fontsize=30)
-# plt.savefig('galaxy_environment.png')
-plt.show()
+plt.savefig('galaxy_environment.png')
+# plt.show()
 
 
 # plt.colorbar(label='separation')
