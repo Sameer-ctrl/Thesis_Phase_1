445d0fc786f3025e611069b558c7669afa815465
Sameer-ctrl <itssameer.patidar@gmail.com>
2023-10-12 16:04:15 +0530

    Just

--- Python/system_plots.py
+++ Python/system_plots.py
@@ -158,30 +158,4 @@ plt.text(0.38, 1.08, f'$\mathbf{{{qso_label} \ (z_{{abs}}={z_abs})}}$', fontsize
 plt.savefig(f'Files_n_figures/{qso_label}_sys_plot.png')
 plt.savefig(f'../VPfit/{qso}/{qso_label}_sys_plot.png')
 
-# plt.show()  -1.5,4.2
-
-
-
-# quit()
-
-
-# t = np.arange(0.01, 5.0, 0.01)
-# s1 = np.sin(2 * np.pi * t)
-# s2 = np.exp(-t)
-# s3 = np.sin(4 * np.pi * t)
-
-# ax1 = plt.subplot(311)
-# plt.plot(t, s1)
-# plt.tick_params('x', labelsize=6)
-
-# # share x only
-# ax2 = plt.subplot(312, sharex=ax1, sharey=ax1)
-# plt.plot(t, s2)
-# # make these tick labels invisible
-# plt.tick_params('x', labelbottom=False)
-
-# # share x and y
-# ax3 = plt.subplot(313, sharex=ax1, sharey=ax1)
-# plt.plot(t, s3)
-# plt.xlim(0.01, 5.0)
-# plt.show()
+# plt.show() 
