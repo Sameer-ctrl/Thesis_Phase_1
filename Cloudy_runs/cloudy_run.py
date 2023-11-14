5fc88d32489ef003d569e21bc284c5fb02188e70
Sameer-ctrl <itssameer.patidar@gmail.com>
2023-11-14 10:46:37 +0530

    HPC morning

--- Cloudy_runs/cloudy_run.py
+++ Cloudy_runs/cloudy_run.py
@@ -108,7 +108,7 @@ def run_cloudy(run_name, hden, metal, temp, redshift, stop_nH, ions,qso, stop_T=
         cloudy_path_workstation='/home/sameer/Sameer/c22.02/source/cloudy.exe'
         cloudy_path_pc='/home/sameer/cloudy/source/sys_gcc/cloudy.exe'
 
-        cloudy_path=cloudy_path_desktop
+        cloudy_path=cloudy_path_workstation
 
         cloudy_run_command=f'{cloudy_path} -r {run_name}'
 
@@ -179,22 +179,21 @@ def run_cloudy(run_name, hden, metal, temp, redshift, stop_nH, ions,qso, stop_T=
 
     else:
 
-        os.chdir(f'{qso}/{run_name}')
-        os.remove(f'{run_name}')
+        os.system(f'rm -rf {qso}/{run_name}')
         print('Cloudy run terminated...')
 
 
-run_name='component_I_PI_nH_Z'
+run_name='component_I_PI_nH'
 qso='pks0637'
-hden=[-5,0,0.02]
-metal=[-3,2,0.05]
+hden=[-5,1,0.02]
+metal=[-1]
 temp=None
 redshift=0.161013
 stop_nH=13.60
 
 ions=['H', 'H+', 'C+','C+2', 'C+3','N+2', 'N+4', 'O','O+2','O+5','O+6','Si+', 'Si+2', 'Si+3','Si+4']
 
-run_cloudy(run_name, hden, metal, temp, redshift, stop_nH, ions, qso)
+run_cloudy(run_name, hden, metal, temp, redshift, stop_nH, ions, qso,save_temp=True,delete_temp_file=True,stop_T=100)
 
 # H	H+	C+	C+0	C+0	N+0	N+0	O	O+0	O+0	O+0	Si+	Si+0	Si+0	Si+0
 # 0.00000e+00	0.00000e+00	0.00000e+00	0.00000e+00	0.00000e+00	0.00000e+00	0.00000e+00	0.00000e+00	0.00000e+00	0.00000e+00	0.00000e+00	0.00000e+00	0.00000e+00	0.00000e+00	0.00000e+00
