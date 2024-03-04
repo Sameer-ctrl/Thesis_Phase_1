504dc9cf39ad45ce3b19d03adba3a56557cbc10d
Sameer-ctrl <itssameer.patidar@gmail.com>
2024-03-04 14:31:52 +0530

    HPC 1607

--- Cloudy_runs/OVI_interpolater.py
+++ Cloudy_runs/OVI_interpolater.py
@@ -6,6 +6,24 @@ from scipy.interpolate import interp2d
 import pickle
 
 
+hdu=fits.open(f'pg0003/z=0.347579/nH_Z/component_III_nH_Z_col_density_param.fits')
+data_2d=Table(hdu[1].data)
+
+hdu=fits.open(f'pg0003/z=0.347579/nH_Z=-1/component_III_nH_col_density_param.fits')
+data_1d=Table(hdu[1].data)
+
+log_nH_2d=data_2d['log_nH']
+log_Z_2d=data_2d['log_Z']
+
+
+# plt.
+
+
+
+quit()
+
+
+
 hdu=fits.open('Data/col_den_OVI.fits')
 data=Table(hdu[1].data)
 
