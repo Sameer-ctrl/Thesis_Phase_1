5f6565614e4294c3da82d313e0a6e311c85e9564
Sameer-ctrl <itssameer.patidar@gmail.com>
2024-03-06 17:02:49 +0530

    Desktop

--- Cloudy_runs/OVI_interpolater.py
+++ Cloudy_runs/OVI_interpolater.py
@@ -1,40 +1,103 @@
 from astropy.io import fits
-from astropy.table import Table
+from astropy.table import Table,Column
 import matplotlib.pyplot as plt
 from numpy import *
-from scipy.interpolate import interp2d
+from scipy.interpolate import interp2d,interp1d
 import pickle
 
 
-hdu=fits.open(f'pg0003/z=0.347579/nH_Z/component_III_nH_Z_col_density_param.fits')
-data_2d=Table(hdu[1].data)
+# hdu=fits.open(f'pg0003/z=0.347579/nH_Z/component_III_nH_Z_col_density_param.fits')
+# data_2d=Table(hdu[1].data)
 
-hdu=fits.open(f'pg0003/z=0.347579/nH_Z=-1/component_III_nH_col_density_param.fits')
-data_1d=Table(hdu[1].data)
+# hdu=fits.open(f'pg0003/z=0.347579/nH_Z=-1/component_III_nH_col_density_param.fits')
+# data_1d=Table(hdu[1].data)
 
-log_nH_2d=data_2d['log_nH']
-log_Z_2d=data_2d['log_Z']
+# mask=data_2d['O+5']>0
 
-ions=['Si+', 'Si+2','C+', 'C+2','O+5']
+# log_nH_2d=data_2d['log_nH'][mask]
+# nH_2d_unique=unique(data_2d['log_nH'])
 
-for i in ions:
+# log_Z_2d=data_2d['log_Z'][mask]
+# col_den_OVI=log10(data_2d['O+5'][mask])
 
-    col_den_OVI=log10(data_2d[i])
-    plt.figure(figsize=(10,10))
-    plt.scatter(log_nH_2d,log_Z_2d,c=col_den_OVI)
-    plt.colorbar()
-    plt.xlim(-5,0)
-    plt.ylim(-3,2)
-    plt.title(i)
-    plt.savefig(f'{i}_obs.png')
 
+# table=Table()
+# logZ_table=[]
+# lognH_table=[]
+# col_den_table=[]
+
+# Z=unique(log_Z_2d)
+
+# for z in Z:
+
+#     mask=log_Z_2d==z
+
+#     if z>=0.64 and z<=0.88 :
+#         nH=log_nH_2d[mask][:-1]
+#         col_den_OVI_masked=col_den_OVI[mask][:-1]
+    
+#     else:
+#         nH=log_nH_2d[mask]
+#         col_den_OVI_masked=col_den_OVI[mask]
+    
+
+#     f=interp1d(nH,col_den_OVI_masked,fill_value='extrapolate',kind='quadratic')
+
+#     logZ=z*ones(len(nH_2d_unique))
+#     col_den=f(nH_2d_unique)
+    
+#     logZ_table.append(logZ)
+#     lognH_table.append(nH_2d_unique)
+#     col_den_table.append(col_den)
+
+
+
+# Z_col=Column(name='log_Z',data=array(logZ_table).flatten())
+# nH_col=Column(name='log_nH',data=array(lognH_table).flatten())
+# col_den_col=Column(name='O+5',data=array(col_den_table).flatten())
+
+# table.add_columns([nH_col,Z_col,col_den_col])
+# table.write('pg0003/z=0.347579/nH_Z/col_den_OVI_extrapolated.fits')
+
+# quit()
+
+hdu=fits.open('pg0003/z=0.347579/nH_Z/col_den_OVI_extrapolated.fits')
+data=Table(hdu[1].data)
+
+log_nH=data['log_nH']
+log_Z=data['log_Z']
+log_col_den=data['O+5']
+
+kind='quintic'
+
+f=interp2d(log_nH,log_Z,log_col_den,kind=kind)
+
+
+with open(f'Interp_2d_func_new/O+5_{kind}.pkl','wb') as pickle_file:
+        pickle.dump(f,pickle_file)
+
+
+# plt.scatter(nH_col,Z_col,c=col_den_col)
+# plt.colorbar()
+# plt.show()
+
+# fig=plt.figure()
+# ax=plt.axes(projection ='3d')   
+
+# ax.scatter(nH_col,Z_col,col_den_col,label='interp1')
+# # ax.scatter(x,y,col_den,label='interpolated')
+# # ax.scatter(x,y,col_den1,label='interpolated2')
+# ax.set_xlabel('nH')
+# ax.set_ylabel('Z')
+# ax.set_zlabel('col den')
+
+# plt.legend()
 
 # plt.show()
 
 
 
 quit()
-
 
 
 hdu=fits.open('Data/col_den_OVI.fits')
