33133fda654902000c72dde28bf15b793d4a6a80
Sameer-ctrl <itssameer.patidar@gmail.com>
2024-05-08 20:05:45 +0530

    Desktop

--- Python/omega_b_BLA.py
+++ Python/omega_b_BLA.py
@@ -19,6 +19,14 @@ data=ascii.read('../Cloudy_runs/ionisation_modelling_sol.txt')
 qso_all=data['qso']
 z_abs_all=data['z_abs']
 
+def z_qso(qso):
+
+    file_systems=open(f'Data/IGM_Danforth_Data/Systems/{qso}_igm-systems.txt','r')
+    z=float(file_systems.readlines()[16].split(' ')[1])
+
+    return z
+
+
 v_z=lambda z : 3e5*(((1+round(z,6))**2-1)/((1+round(z,6))**2+1))  # v at z
 err_vz=lambda z,z_err: 4*3e5*((1+round(z,6))/(((1+round(z,6))**2)+1)**2)*round(z_err,6)
 z_v=lambda v : sqrt((1+((v)/3e5))/(1-((v)/3e5)))-1      # z at v
@@ -347,41 +355,41 @@ def omega_BLA(qso,b,N,err_b,err_N,T=None,err_T=None):
 
     err_omega_BLA=log(10)*(A/delta_X)*sqrt(sum((10**(2*(fH+N)))*(err_fH**2+err_N**2)))
 
-    return omega_BLA_val,err_omega_BLA
+    return omega_BLA_val,err_omega_BLA,log10(NH),delta_X
 
 b_BLA_th=40
 
-absorbers=[
-            abs_system('3c263',0.140756),
-            abs_system('pks0637',0.161064),
-            abs_system('pks0637',0.417539),
-            abs_system('pg1424',0.147104),
-            abs_system('pg0003',0.347586),                        
-            abs_system('pg0003',0.386089),
-            abs_system('pg0003',0.421923),
-            abs_system('pg1216',0.282286),
-            abs_system('s135712',0.097869),
-            abs_system('1es1553',0.187764),
-            abs_system('sbs1108',0.463207),
-            abs_system('pg1222',0.378389),
-            abs_system('pg1116',0.138527),
-            abs_system('h1821',0.170006),
-            abs_system('h1821',0.224981),
-            abs_system('pg1121',0.192393),
-            abs_system('pks0405',0.167125),
-            abs_system('he0056',0.043265),
-            abs_system('pg1216',0.006328),     #
-            abs_system('3c263',0.063397),
-            abs_system('pg1222',0.054479),
-            abs_system('rxj0439',0.005568),                        
-            abs_system('uks0242',0.063850),    #
-            abs_system('pg1259',0.046284),
-            abs_system('pks1302',0.094839),
-            abs_system('3c57',0.077430),
-            abs_system('p1103',0.003934),
-            abs_system('phl1811',0.080928),
-            abs_system('pg0832',0.017505,cont_mark='^',fix_param_mark='B')      #
-           ]
+# absorbers=[
+#             abs_system('3c263',0.140756),
+#             abs_system('pks0637',0.161064),
+#             abs_system('pks0637',0.417539),
+#             abs_system('pg1424',0.147104),
+#             abs_system('pg0003',0.347586),                        
+#             abs_system('pg0003',0.386089),
+#             abs_system('pg0003',0.421923),
+#             abs_system('pg1216',0.282286),
+#             abs_system('s135712',0.097869),
+#             abs_system('1es1553',0.187764),
+#             abs_system('sbs1108',0.463207),
+#             abs_system('pg1222',0.378389),
+#             abs_system('pg1116',0.138527),
+#             abs_system('h1821',0.170006),
+#             abs_system('h1821',0.224981),
+#             abs_system('pg1121',0.192393),
+#             abs_system('pks0405',0.167125),
+#             abs_system('he0056',0.043265),
+#             abs_system('pg1216',0.006328),     #
+#             abs_system('3c263',0.063397),
+#             abs_system('pg1222',0.054479),
+#             abs_system('rxj0439',0.005568),                        
+#             abs_system('uks0242',0.063850),    #
+#             abs_system('pg1259',0.046284),
+#             abs_system('pks1302',0.094839),
+#             abs_system('3c57',0.077430),
+#             abs_system('p1103',0.003934),
+#             abs_system('phl1811',0.080928),
+#             abs_system('pg0832',0.017505,cont_mark='^',fix_param_mark='B')      #
+#            ]
 
 # print([a.z_abs for a in absorbers])
 # print([a.qso for a in absorbers])
@@ -402,7 +410,6 @@ absorbers=[
 # plt.show()
 
 
-quit()
 # absorbers=[
 #             abs_system('pg0003',0.347586),                        
 #             abs_system('pg0003',0.386089),
@@ -413,47 +420,59 @@ quit()
 #             abs_system('pg1121',0.192393),
 #            ]
 
-BLA_dict={}
-z_abs=[]
+# BLA_dict={}
+# z_abs=[]
 
-for a in absorbers:
-    BLA_obj=a.BLA_obj
-    b=BLA_obj.b
+# for a in absorbers:
+#     BLA_obj=a.BLA_obj
+#     b=BLA_obj.b
 
-    if len(b)>0:
-        BLA_dict[a.qso]=[[],[],[],[],[],[]]
-        z_abs.append(a.z_abs)
+#     if len(b)>0:
+#         BLA_dict[a.qso]=[[],[],[],[],[],[]]
+#         z_abs.append(a.z_abs)
 
-z_abs=unique(z_abs)
+# z_abs=unique(z_abs)
 
-for a in absorbers:
-    BLA_obj=a.BLA_obj
-    b=BLA_obj.b
-    N=BLA_obj.logN
+# for a in absorbers:
+#     BLA_obj=a.BLA_obj
+#     b=BLA_obj.b
+#     N=BLA_obj.logN
 
-    if len(b)>0:
+#     if len(b)>0:
 
-        b_val=[]
-        b_err=[]
-        N_val=[]
-        N_err=[]
+#         b_val=[]
+#         b_err=[]
+#         N_val=[]
+#         N_err=[]
 
-        for i in range(len(b)):
-            b_val.append(b[i][0])
-            b_err.append(b[i][1])
-            N_val.append(N[i][0])
-            N_err.append(N[i][1])
+#         for i in range(len(b)):
+#             b_val.append(b[i][0])
+#             b_err.append(b[i][1])
+#             N_val.append(N[i][0])
+#             N_err.append(N[i][1])
 
-        BLA_dict[a.qso][0]+=b_val
-        BLA_dict[a.qso][1]+=N_val
-        BLA_dict[a.qso][2]+=b_err
-        BLA_dict[a.qso][3]+=N_err
+#         BLA_dict[a.qso][0]+=b_val
+#         BLA_dict[a.qso][1]+=N_val
+#         BLA_dict[a.qso][2]+=b_err
+#         BLA_dict[a.qso][3]+=N_err
+
+# qso=list(BLA_dict.keys())
+# print(BLA_dict)
+
+'Sample A : 5'
+
+BLA_dict={'pg0003': [[63], [14.2], [3], [0.02],[5.28],[0.05]], '1es1553': [[51], [13.88], [1], [0.01],[5.19],[0.04]], 'pg1222': [[52], [14.34], [4], [0.05],[5],[0.17]], 'pg1116': [[71], [13.6], [14], [0.23],[5.39],[0.21]], 'h1821': [[84], [13.64], [13], [0.11],[5.51],[0.16]]}
+
+'Sample A : 7'
+
+# BLA_dict={'pg0003': [[63, 40], [14.2, 14.1], [0, 4], [0.02, 0.05],[5.28,4.80],[0.05,0.11]], '1es1553': [[51], [13.88], [1], [0.01],[5.19],[0.04]], 'pg1222': [[52, 43], [14.34, 15.43], [4, 1], [0.05, 0.04],[5,4.81],[0.17,0.19]], 'pg1116': [[71], [13.6], [14], [0.23],[5.39],[0.21]], 'h1821': [[84], [13.64], [13], [0.11],[5.51],[0.16]]}
+
+'Sample A : 8'
+
+# BLA_dict={'pg0003': [[63, 40], [14.2, 14.1], [0, 4], [0.02, 0.05],[5.28,4.80],[0.05,0.11]], '1es1553': [[51], [13.88], [1], [0.01],[5.19],[0.04]], 'pg1222': [[52, 43], [14.34, 15.43], [4, 1], [0.05, 0.04],[5,4.81],[0.17,0.19]], 'pg1116': [[71], [13.6], [14], [0.23],[5.39],[0.21]], 'h1821': [[84], [13.64], [13], [0.11],[5.51],[0.16]],'pg1121': [[60], [14.34], [6], [0.09],[5.34],[0.20]]}
+
 
 qso=list(BLA_dict.keys())
-# print(BLA_dict)
-# BLA_dict={'pg0003': [[63, 40], [14.2, 14.1], [0, 4], [0.02, 0.05],[5.28,4.80],[0.05,0.11]], '1es1553': [[51], [13.88], [1], [0.01],[5.19],[0.04]], 'pg1222': [[52, 43], [14.34, 15.43], [4, 1], [0.05, 0.04],[5,4.81],[0.17,0.19]], 'pg1116': [[71], [13.6], [14], [0.23],[5.39],[0.21]], 'h1821': [[84], [13.64], [13], [0.11],[5.51],[0.16]]}
-# # , 'pg1121': [[60], [14.34], [6], [0.09],[5.35],[0.14]]
-# qso=list(BLA_dict.keys())
 
 
 h=0.7
@@ -486,13 +505,15 @@ for i,q in enumerate(qso):
     N_all_err+=N_los_err
     T_all_err+=T_los_err
 
-    val, err = omega_BLA(q,b_los,N_los,b_los_err,N_los_err)
-    # val, err = omega_BLA(q,b_los,N_los,b_los_err,N_los_err,T=T_los,err_T=T_los_err)
+    # val, err = omega_BLA(q,b_los,N_los,b_los_err,N_los_err)
+    val, err,NH, dX = omega_BLA(q,b_los,N_los,b_los_err,N_los_err,T=T_los,err_T=T_los_err)
 
     omega_BLA_los.append(val*100)
     omega_BLA_los_err.append(err*100)
 
-    print(q, round(val*100,2))
+    # print(q, f'{round(val*100,2):.2f}')
+    print(f'{qso_dict[q]}  &  {z_qso(q):.3f}  & {NH:.2f}  &  {dX:.3f} & {round(val*100,2):.2f} $\pm$ {round(err*100,2):.2f} \\\\')
+
 
 # qso=['3c263', 'pks0637', 'pg1424', 'pg0003', 'pg1216', 's135712', '1es1553', 'pg1222', 'pg1116', 'h1821', 'pg1121', 'pks0405']
 # b_all=[87, 162, 45, 46, 40, 63, 40, 66, 64, 52, 53, 46, 51, 64, 52, 43, 71, 63, 84, 62, 60, 56]
@@ -501,9 +522,10 @@ for i,q in enumerate(qso):
 # N_all_err=[0.06, 0.06, 0.02, 0.07, 0.02, 0.02, 0.05, 0.05, 0.04, 0.05, 0.18, 0.16, 0.01, 0.11, 0.05, 0.04, 0.23, 0.02, 0.11, 0.06, 0.09, 0.06]
 
 
-omega_BLA_all, omega_BLA_all_err = omega_BLA(qso,b_all,N_all,b_all_err,N_all_err)
-# omega_BLA_all, omega_BLA_all_err = omega_BLA(qso,b_all,N_all,b_all_err,N_all_err,T=T_all,err_T=T_all_err)
-print(f'\u03A9 = {round(omega_BLA_all*100,2)} \u00B1 {round(omega_BLA_all_err*100,2)}')
+# omega_BLA_all, omega_BLA_all_err = omega_BLA(qso,b_all,N_all,b_all_err,N_all_err)
+omega_BLA_all, omega_BLA_all_err,NH, dX = omega_BLA(qso,b_all,N_all,b_all_err,N_all_err,T=T_all,err_T=T_all_err)
+print(f'\n\hline \n \nTotal &  &  {NH:.2f} &  {dX:.3f}  & {round(omega_BLA_all*100,2):.2f} $\pm$  {round(omega_BLA_all_err*100,2):.2f} \\\\ \n')
+print(f'\u03A9 = {round(omega_BLA_all*100,2):.2f} \u00B1 {round(omega_BLA_all_err*100,2):.2f},  dX={dX:.3f} NH={NH:.2f}')
 
 # plt.figure()
 
